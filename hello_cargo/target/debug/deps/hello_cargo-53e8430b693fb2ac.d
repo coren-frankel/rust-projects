4b037200f0ee83f5eba74159de53a7fed39e301d
unclebabykern <coren.frankel@gmail.com>
2025-02-04 14:49:10 -0700

    chore: remove target dir after moving .gitignore

--- hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d
+++
@@ -1,5 +0,0 @@
-/Users/Coren/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac: src/main.rs
-
-/Users/Coren/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/Coren/repos/rust-projects/hello_cargo/target/debug/hello_cargo: /Users/Coren/repos/rust-projects/hello_cargo/src/main.rs
