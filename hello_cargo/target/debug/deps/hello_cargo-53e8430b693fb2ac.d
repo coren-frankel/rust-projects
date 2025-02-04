cd84755a215e0692cf81a9eeddc4d8052e562380
unclebabykern <coren.frankel@gmail.com>
2025-02-04 14:45:24 -0700

    Initial commit; through chapter 1 of The Rust Programming Language

---
+++ hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d
@@ -0,0 +1,5 @@
+/Users/Coren/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac: src/main.rs
+
+/Users/Coren/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/Coren/repos/rust-projects/hello_cargo/target/debug/hello_cargo: /Users/Coren/repos/rust-projects/hello_cargo/src/main.rs
